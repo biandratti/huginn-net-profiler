c4627ffb60d29bd00883415bdb2279fcc63502c1
biandratti <maxibiandra@gmail.com>
2025-08-03 10:07:57 +0200

    docker compose fixed

--- profiler/http-collector/src/main.rs
+++ profiler/http-collector/src/main.rs
@@ -1,151 +1,160 @@
-use anyhow::Result;
 use clap::Parser;
 use huginn_net::{db::Database, fingerprint_result::FingerprintResult, HuginnNet};
-use log::{error, info, warn};
-use serde::Serialize;
+use log::{error, info};
+use serde::{Deserialize, Serialize};
+use std::env;
 use std::sync::mpsc as std_mpsc;
+use std::thread;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tokio::runtime::Runtime;
 use tokio::sync::mpsc as tokio_mpsc;
-use tokio::signal;
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)]
 struct Args {
-    #[clap(short, long, value_parser, default_value = "eth0")]
-    interface: String,
+    #[clap(short, long, value_parser)]
+    interface: Option<String>,
     #[clap(
         short,
         long,
         value_parser,
-        default_value = "http://localhost:8000/api/ingest"
+        default_value = "http://profile-assembler:8000/api/ingest"
     )]
     assembler_endpoint: String,
 }
 
-#[derive(Serialize, Debug)]
-struct HttpIngest {
-    source_ip: String,
-    signature: HttpSignature,
-}
-
-#[derive(Serialize, Debug)]
-struct HttpSignature {
-    browser: String,
-    os: String,
-}
-
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct TcpIngest {
-    source_ip: String,
-    signature: TcpSignature,
+    id: String,
+    timestamp: u64,
+    tcp_signature: String,
+    os: String,
+    nat: bool,
 }
 
-#[derive(Serialize, Debug)]
-struct TcpSignature {
+#[derive(Serialize, Deserialize, Debug)]
+struct HttpIngest {
+    id: String,
+    timestamp: u64,
+    http_signature: String,
     os: String,
     browser: String,
 }
 
-fn spawn_channel_bridge(
-    sync_rx: std_mpsc::Receiver<FingerprintResult>,
-    async_tx: tokio_mpsc::Sender<FingerprintResult>,
-) {
-    std::thread::spawn(move || {
-        for result in sync_rx.iter() {
-            if async_tx.blocking_send(result).is_err() {
-                error!("Failed to bridge event: async channel closed.");
+fn main() {
+    env_logger::init();
+    let args = Args::parse();
+    let interface = args
+        .interface
+        .unwrap_or_else(|| env::var("PROFILER_INTERFACE").unwrap_or("wlp0s20f3".to_string()));
+    let assembler_endpoint = args.assembler_endpoint;
+
+    info!(
+        "Booting http-collector on interface {} pointed to {}",
+        interface, assembler_endpoint
+    );
+
+    let (sync_tx, sync_rx) = std_mpsc::channel::<FingerprintResult>();
+    let (async_tx, mut async_rx) = tokio_mpsc::channel(1000);
+
+    thread::spawn(move || {
+        while let Ok(item) = sync_rx.recv() {
+            if async_tx.blocking_send(item).is_err() {
+                error!("Failed to send fingerprint to async processor. Channel closed.");
                 break;
             }
         }
     });
-}
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    env_logger::init();
-    let args = Args::parse();
+    let analysis_interface = interface.clone();
+    thread::spawn(move || loop {
+        info!("Starting new HTTP analysis loop on interface {}...", analysis_interface);
+        let db = Box::leak(Box::new(Database::default()));
+        let mut huginn = HuginnNet::new(Some(db), 1024, None);
 
-    info!("Starting HTTP/TCP collector on interface {}", args.interface);
-
-    let (sync_tx, sync_rx) = std_mpsc::channel();
-    let (async_tx, mut async_rx) = tokio_mpsc::channel(1000);
-
-    spawn_channel_bridge(sync_rx, async_tx);
-
-    let interface = args.interface.clone();
-    std::thread::spawn(move || {
-        loop {
-            info!("Starting new HTTP/TCP analysis loop on interface {}...", interface);
-            let db = Box::leak(Box::new(Database::default()));
-            let mut huginn = HuginnNet::new(Some(db), 1024, None);
-        
-            if let Err(e) = huginn.analyze_network(&interface, sync_tx.clone()) {
-                error!("Huginn-net (HTTP/TCP) analysis failed: {}. Restarting in 5 seconds...", e);
-            } else {
-                info!("Huginn-net (HTTP/TCP) analysis finished. Restarting in 5 seconds...");
-            }
-            std::thread::sleep(std::time::Duration::from_secs(5));
+        if let Err(e) = huginn.analyze_network(&analysis_interface, sync_tx.clone()) {
+            error!(
+                "Huginn-net (HTTP) analysis failed: {}. Restarting in 5 seconds...",
+                e
+            );
+            thread::sleep(Duration::from_secs(5));
+        } else {
+            info!("HTTP analysis loop finished cleanly. Restarting immediately.");
         }
     });
 
-    let client = reqwest::Client::new();
-    let assembler_endpoint_http = format!("{}/http", args.assembler_endpoint);
-    let assembler_endpoint_tcp = format!("{}/tcp", args.assembler_endpoint);
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        let client = reqwest::Client::new();
+        info!("Starting HTTP result processor...");
 
-    let processing_task = tokio::spawn(async move {
         while let Some(result) = async_rx.recv().await {
-            
+            let now = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap_or_default()
+                .as_secs();
+
             if let Some(tcp_data) = result.syn {
                 if let Some(os_match) = tcp_data.os_matched {
-                    let tcp_ingest = TcpIngest {
-                        source_ip: tcp_data.source.ip.to_string(),
-                        signature: TcpSignature {
-                            os: os_match.os.name.to_string(),
-                            browser: "".to_string(), // TCP doesn't provide browser info
-                        },
+                    let ingest = TcpIngest {
+                        id: format!("{}:{}", tcp_data.source.ip, tcp_data.source.port),
+                        timestamp: now,
+                        tcp_signature: tcp_data.sig.to_string(),
+                        os: os_match.os.name.to_string(),
+                        nat: false, // NAT info not available in this struct
                     };
-                    send_data(&client, &assembler_endpoint_tcp, tcp_ingest).await;
+                    send_tcp_to_assembler(ingest, &client, &assembler_endpoint).await;
                 }
             }
+
             if let Some(http_data) = result.http_request {
                 if let Some(browser_match) = http_data.browser_matched {
-                    let http_ingest = HttpIngest {
-                        source_ip: http_data.source.ip.to_string(),
-                        signature: HttpSignature {
-                            os: "".to_string(), // OS info comes from TCP fingerprint
-                            browser: browser_match.browser.name.to_string(),
-                        },
+                    let ingest = HttpIngest {
+                        id: format!("{}:{}", http_data.source.ip, http_data.source.port),
+                        timestamp: now,
+                        http_signature: http_data.sig.to_string(),
+                        os: "".to_string(), // OS info comes from TCP fingerprint
+                        browser: browser_match.browser.name.to_string(),
                     };
-                    send_data(&client, &assembler_endpoint_http, http_ingest).await;
+                    send_http_to_assembler(ingest, &client, &assembler_endpoint).await;
                 }
             }
         }
     });
-
-    info!("HTTP/TCP collector is running. Press Ctrl+C to stop.");
-    signal::ctrl_c().await.expect("Failed to listen for Ctrl+C");
-    info!("Ctrl+C received, shutting down.");
-
-    processing_task.abort();
-    Ok(())
 }
 
-async fn send_data<T: Serialize>(client: &reqwest::Client, endpoint: &str, data: T) {
-    let source_ip_for_log = serde_json::to_string(&data).unwrap_or_default();
-    info!("Sending data to assembler {}: {}", endpoint, source_ip_for_log);
-    match client.post(endpoint).json(&data).send().await {
+async fn send_tcp_to_assembler(data: TcpIngest, client: &reqwest::Client, endpoint: &str) {
+    info!("Sending TCP data for {}", data.id);
+    let url = format!("{}/tcp", endpoint);
+    match client.post(&url).json(&data).send().await {
         Ok(response) => {
-            if response.status().is_success() {
-                info!("Successfully sent data for {}.", source_ip_for_log);
-            } else {
-                warn!(
-                    "Failed to send data for {}. Status: {}",
-                   source_ip_for_log,
-                    response.status()
+            if !response.status().is_success() {
+                error!(
+                    "Failed to send TCP data for {}. Status: {}, Body: {:?}",
+                    data.id,
+                    response.status(),
+                    response.text().await
                 );
             }
         }
-        Err(e) => {
-            error!("Error sending data for {}: {:?}", source_ip_for_log, e);
+        Err(e) => error!("Error sending TCP data for {}: {:?}", data.id, e),
+    }
+}
+
+async fn send_http_to_assembler(data: HttpIngest, client: &reqwest::Client, endpoint: &str) {
+    info!("Sending HTTP data for {}", data.id);
+    let url = format!("{}/http", endpoint);
+    match client.post(&url).json(&data).send().await {
+        Ok(response) => {
+            if !response.status().is_success() {
+                error!(
+                    "Failed to send HTTP data for {}. Status: {}, Body: {:?}",
+                    data.id,
+                    response.status(),
+                    response.text().await
+                );
+            }
         }
+        Err(e) => error!("Error sending HTTP data for {}: {:?}", data.id, e),
     }
 } 
--- profiler/tls-collector/src/main.rs
+++ profiler/tls-collector/src/main.rs
@@ -1,112 +1,124 @@
-use anyhow::Result;
 use clap::Parser;
 use huginn_net::{db::Database, fingerprint_result::FingerprintResult, HuginnNet};
-use log::{error, info, warn};
-use serde::Serialize;
+use log::{error, info};
+use serde::{Deserialize, Serialize};
+use std::env;
 use std::sync::mpsc as std_mpsc;
-use tokio::signal;
+use std::thread;
+use std::time::Duration;
+use tokio::runtime::Runtime;
 use tokio::sync::mpsc as tokio_mpsc;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)]
 struct Args {
-    #[clap(short, long, value_parser, default_value = "wlp0s20f3")]
-    interface: String,
+    #[clap(short, long, value_parser)]
+    interface: Option<String>,
     #[clap(
         short,
         long,
         value_parser,
-        default_value = "http://localhost:8000/api/ingest/tls"
+        default_value = "http://profile-assembler:8000/api/ingest/tls"
     )]
     assembler_endpoint: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct TlsIngest {
-    correlation_id: String,
-    ja4: String,
+    id: String,
+    timestamp: u64,
+    ja4_fingerprint: String,
+    ja4_hash: String,
 }
 
-fn spawn_channel_bridge(
-    sync_rx: std_mpsc::Receiver<FingerprintResult>,
-    async_tx: tokio_mpsc::Sender<FingerprintResult>,
-) {
-    std::thread::spawn(move || {
-        for result in sync_rx.iter() {
-            if async_tx.blocking_send(result).is_err() {
-                error!("Failed to bridge event: async channel closed.");
+fn main() {
+    env_logger::init();
+    let args = Args::parse();
+    let interface = args.interface.unwrap_or_else(|| {
+        env::var("PROFILER_INTERFACE").unwrap_or("wlp0s20f3".to_string())
+    });
+    let assembler_endpoint = args.assembler_endpoint;
+
+    info!(
+        "Booting tls-collector on interface {} pointed to {}",
+        interface, assembler_endpoint
+    );
+
+    // Create a channel for sync communication (huginn-net to our bridge)
+    let (sync_tx, sync_rx) = std_mpsc::channel::<FingerprintResult>();
+
+    // Create a channel for async communication (our bridge to tokio tasks)
+    let (async_tx, mut async_rx) = tokio_mpsc::channel(1000);
+
+    // Bridge the sync and async worlds
+    thread::spawn(move || {
+        while let Ok(item) = sync_rx.recv() {
+            if async_tx.blocking_send(item).is_err() {
+                error!("Failed to send fingerprint to async processor. Channel closed.");
                 break;
             }
         }
     });
-}
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    env_logger::init();
-    let args = Args::parse();
-    info!("Starting TLS collector on interface {}", args.interface);
+    // Start the network analysis in its own thread
+    let analysis_interface = interface.clone();
+    thread::spawn(move || loop {
+        info!("Starting new TLS analysis loop on interface {}...", analysis_interface);
+        let db = Box::leak(Box::new(Database::default()));
+        let mut huginn = HuginnNet::new(Some(db), 1024, None);
 
-    let (sync_tx, sync_rx) = std_mpsc::channel();
-    let (async_tx, mut async_rx) = tokio_mpsc::channel(1000);
-
-    spawn_channel_bridge(sync_rx, async_tx);
-
-    let interface = args.interface.clone();
-    std::thread::spawn(move || {
-        loop {
-            info!("Starting new TLS analysis loop on interface {}...", interface);
-            let db = Box::leak(Box::new(Database::default()));
-            let mut huginn = HuginnNet::new(Some(db), 1024, None);
-
-            if let Err(e) = huginn.analyze_network(&interface, sync_tx.clone()) {
-                error!("Huginn-net (TLS) analysis failed: {}. Restarting in 5 seconds...", e);
-            } else {
-                info!("Huginn-net (TLS) analysis finished. Restarting in 5 seconds...");
-            }
-            std::thread::sleep(std::time::Duration::from_secs(5));
+        if let Err(e) = huginn.analyze_network(&analysis_interface, sync_tx.clone()) {
+            error!(
+                "Huginn-net (TLS) analysis failed: {}. Restarting in 5 seconds...",
+                e
+            );
+            thread::sleep(Duration::from_secs(5));
+        } else {
+            info!("TLS analysis loop finished cleanly. Restarting immediately.");
         }
     });
 
-    let client = reqwest::Client::new();
-    let assembler_endpoint = args.assembler_endpoint;
+    // Create a tokio runtime to process results
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        let client = reqwest::Client::new();
+        info!("Starting TLS result processor...");
 
-    let processing_task = tokio::spawn(async move {
         while let Some(result) = async_rx.recv().await {
             if let Some(tls_data) = result.tls_client {
-                let tls_ingest = TlsIngest {
-                    correlation_id: format!("{}:{}", tls_data.source.ip, tls_data.source.port),
-                    ja4: tls_data.sig.ja4.full.value().to_string(),
+                let now = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap_or_default()
+                    .as_secs();
+
+                let ingest = TlsIngest {
+                    id: format!("{}:{}", tls_data.source.ip, tls_data.source.port),
+                    timestamp: now,
+                    ja4_fingerprint: tls_data.sig.ja4.full.value().to_string(),
+                    ja4_hash: tls_data.sig.ja4.ja4_a.to_string(),
                 };
-                send_tls_to_assembler(tls_ingest, &client, &assembler_endpoint).await;
+                send_tls_to_assembler(ingest, &client, &assembler_endpoint).await;
             }
         }
     });
-    
-    info!("TLS collector is running. Press Ctrl+C to stop.");
-    signal::ctrl_c().await.expect("Failed to listen for Ctrl+C");
-    info!("Ctrl+C received, shutting down.");
-    
-    processing_task.abort();
-    Ok(())
 }
 
 async fn send_tls_to_assembler(data: TlsIngest, client: &reqwest::Client, endpoint: &str) {
-    info!("Sending TLS data for {}", data.correlation_id);
+    info!("Sending TLS data for {}", data.id);
     match client.post(endpoint).json(&data).send().await {
         Ok(response) => {
-            if response.status().is_success() {
-                info!("Successfully sent TLS data for {}.", data.correlation_id);
-            } else {
-                warn!(
-                    "Failed to send TLS data for {}. Status: {}",
-                    data.correlation_id,
-                    response.status()
+            if !response.status().is_success() {
+                error!(
+                    "Failed to send TLS data for {}. Status: {}, Body: {:?}",
+                    data.id,
+                    response.status(),
+                    response.text().await
                 );
             }
         }
         Err(e) => {
-            error!("Error sending TLS data for {}: {:?}", data.correlation_id, e);
+            error!("Error sending TLS data for {}: {:?}", data.id, e);
         }
     }
 } 
